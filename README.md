d74cbbc15915fc190e50d3126ad2f5d6bbd503c6
Christian Mahnke <cmahnke@gmail.com>
2021-05-02 18:01:24 +0200

    Initial chackin

---
+++ README.md
@@ -0,0 +1,8 @@
+Experimental Docker image for map creation
+==========================================
+
+# Building
+
+```
+DOCKER_BUILDKIT=1 docker build -t ghcr.io/cmahnke/map-action:latest .
+```
---
+++ scripts/create-map.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+OPENMAPTILES_GIT=https://github.com/openmaptiles/openmaptiles.git
+OPENMAPTILES_TAG="v3.12.2"
+
+git clone $OPENMAPTILES_GIT --branch $OPENMAPTILES_TAG --single-branch
+cd openmaptiles
+docker run -v $(pwd):/tileset openmaptiles/openmaptiles-tools make
+docker-compose up -d postgres
+docker-compose run import-water
+docker-compose run import-natural-earth
+docker-compose run import-lakelines
+docker-compose run import-osmborder
