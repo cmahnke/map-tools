84152ea0031761dc5e4a8c89515a0879d1b7c8f9
Christian Mahnke <cmahnke@gmail.com>
2023-09-24 19:12:37 +0200

    Moved data to another repository

--- docker/data/scripts/get-osmosis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-NAME=`date |md5`
-
-docker pull ghcr.io/cmahnke/map-tools/osmosis:latest
-docker create --name $NAME ghcr.io/cmahnke/map-tools/osmosis:latest
-docker cp $NAME:/opt/osmosis .
-docker rm -f $NAME
