065bde046838cbea8114a86605ca993f459504da
Christian Mahnke <cmahnke@gmail.com>
2024-01-06 09:29:34 +0100

    Create missing directory

--- docker/planetiler/scripts/get-data.sh
+++ docker/planetiler/scripts/get-data.sh
@@ -1,6 +1,10 @@
 #/bin/sh
 
-cd "$PLANETILER_DATA_DIR/data/sources/"
+DOWNLOAD_DIR="$PLANETILER_DATA_DIR/data/sources/"
+
+mkdir -p "$DOWNLOAD_DIR"
+cd "$DOWNLOAD_DIR"
+echo "Downloading to `pwd`"
 wget https://osmdata.openstreetmap.de/download/water-polygons-split-3857.zip
 wget https://dev.maptiler.download/geodata/omt/lake_centerline.shp.zip
 wget https://dev.maptiler.download/geodata/omt/natural_earth_vector.sqlite.zip
