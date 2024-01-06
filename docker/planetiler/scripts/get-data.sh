1cf432c8e59d7c40349a1366b62cd2ca1cb92e96
Christian Mahnke <cmahnke@gmail.com>
2024-01-06 19:44:20 +0100

    Reduce log flooding

--- docker/planetiler/scripts/get-data.sh
+++ docker/planetiler/scripts/get-data.sh
@@ -5,7 +5,7 @@ DOWNLOAD_DIR="$PLANETILER_DATA_DIR/data/sources/"
 mkdir -p "$DOWNLOAD_DIR"
 cd "$DOWNLOAD_DIR"
 echo "Downloading to `pwd`"
-wget https://osmdata.openstreetmap.de/download/water-polygons-split-3857.zip
-wget https://dev.maptiler.download/geodata/omt/lake_centerline.shp.zip
-wget https://dev.maptiler.download/geodata/omt/natural_earth_vector.sqlite.zip
+wget --progress=dot:giga https://osmdata.openstreetmap.de/download/water-polygons-split-3857.zip
+wget --progress=dot:giga https://dev.maptiler.download/geodata/omt/lake_centerline.shp.zip
+wget --progress=dot:giga https://dev.maptiler.download/geodata/omt/natural_earth_vector.sqlite.zip
 ln -s $PLANETILER_DATA_DIR/data $PLANETILER_DIR/data
